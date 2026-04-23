ad4134850ba9b06673de8d96124cdde745999c63
abrassat <alexandru.brassat@xwiki.com>
2026-04-23 14:03:48 +0300

    Fix merge (part 2)

--- xwiki-platform-core/xwiki-platform-node/src/main/node/core/guidedtour/guidedtour-ui/vite.config.ts
+++ xwiki-platform-core/xwiki-platform-node/src/main/node/core/guidedtour/guidedtour-ui/vite.config.ts
@@ -18,6 +18,6 @@
  * 02110-1301 USA, or see the FSF site: http://www.fsf.org.
  */
 
-import { generateConfig } from "@xwiki/platform-tool-viteconfig";
+import { generateConfigVue } from "@xwiki/platform-tool-viteconfig";
 
-export default generateConfig(import.meta.url);
+export default generateConfigVue(import.meta.url);
